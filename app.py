98f51332eb682d22777ae96df962b832466306e7
21DP4MSmit <103188916+21DP4MSmit@users.noreply.github.com>
2025-06-03 13:31:57 +0300

    Feat: Deployment

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
@@ -8,9 +9,7 @@ from models import create_tables
 
 CORS(
     app,
-    resources={
-        r"/api/*": {"origins": ["http://localhost:5173", "http://127.0.0.1:5173"]}
-    },
+    resources={r"/api/*": {"origins": "*"}},
     supports_credentials=True,
     methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["Content-Type", "Authorization", "X-Confidence-Threshold"],
@@ -23,6 +22,12 @@ app.config["JWT_COOKIE_CSRF_PROTECT"] = False
 app.config["JWT_SECRET_KEY"] = (
     "24dbdf01c1042bf4d7e55a223ef8fd479a6964308c1fd491d092172fe062c8b2"
 )
+
+if os.environ.get("DATABASE_URL"):
+    app.config["MYSQL_HOST"] = os.environ.get("MYSQL_HOST")
+    app.config["MYSQL_USER"] = os.environ.get("MYSQL_USER")
+    app.config["MYSQL_PASSWORD"] = os.environ.get("MYSQL_PASSWORD")
+    app.config["MYSQL_DB"] = os.environ.get("MYSQL_DB")
 
 jwt = JWTManager(app)
 
@@ -39,4 +44,5 @@ with app.app_context():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
